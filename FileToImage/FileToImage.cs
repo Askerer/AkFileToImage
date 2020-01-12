c04fa869a0f51a9a9bb1b4b92508fc9f014b75e5
Asker <au3wk4@gmail.com>
2020-01-13 00:40:21 +0800

    Excel and Word to Image

---
+++ FileToImage/FileToImage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using PPT = Microsoft.Office.Interop.PowerPoint;
+using Word = Microsoft.Office.Interop.Word;
+
+
+namespace FileToImage
+{
+    public class FileTransform
+    {
+		public void ExcelToJPG(string sFileName)
+		{
+			try
+			{
+				Excel.Application xlsApp = new Excel.Application();
+				Excel.Workbook xlsBook = xlsApp.Workbooks.Open(sFileName);
+				xlsBook.SaveAs(Application.StartupPath + @"\" + sFileName, Excel.XlFileFormat.xlHtml);
+				xlsBook.Close(false, Type.Missing, Type.Missing);
+				xlsApp.Quit();
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.StackTrace.ToString());
+			}
+
+		}
+
+		public void WordToJPG(string sPath)
+		{
+			Word.Application docApp = new Word.Application();
+			Word.Document doc = docApp.Documents.Open(@"D:\ImgW.docx");
+			doc.SaveAs2(Application.StartupPath + @"\ImgW.html", Word.WdSaveFormat.wdFormatHTML);
+			doc.Close();
+			docApp.Quit();
+		}
+
+		//xlsbook.LoadFromFile(@"D:\Images.xlsx");
+		
+
+		
+
+    }
+}
---
+++ TestFileToImg/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileToImage;
+
+namespace TestFileToImg
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FileToImage.FileTransform a = new FileTransform();
+			//a.ExcelToJPG("");
+			a.WordToJPG("");
+
+		}
+	}
+}
